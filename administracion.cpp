8fc77b1d33b66c1a3f8085834cf0d84663b23682
Emmanuel <emmanuel.arnedo@outlook.com.ar>
2020-12-11 23:12:50 -0300

    Update administracion.cpp
    Continuacion modulo administracón

--- administracion.cpp
+++ administracion.cpp
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct registro
-{
+struct registro{
 	char usuario[10];
 	char Ausuario[10];
 	char contra[10];
@@ -12,25 +11,24 @@ struct registro
 	int matricula;
 	int dni;
 	char telefono[25];
+	int modulo;
 };
 
 int dibujarMenu();
-void regusuario(int &creg);
+void regusuario();
 
-main()
-{
-	int opc, creg=0;
-	
+main(){
+	int opc;
 	do{
 		opc = dibujarMenu();
 		switch(opc){
 			case 1:				 	 
 					 break;
 					 
-			case 2:	 regusuario(creg);
+			case 2:	 regusuario();
 					 break;
 					 
-			case 3:				 
+			case 3:			 
 					 break;
 					 
 			case 4:
@@ -45,29 +43,30 @@ int dibujarMenu(){
 	system("cls");
 	printf("*********************VENTAS*************************\n");
 	printf("\t 1) Registrar Veterinario\n");
-	printf("\t 2) Registrar Usuario Asistente\n");
+	printf("\t 2) Registrar Usuario Asistente o Adiminstrador\n");	
 	printf("\t 3) Atenciones por Veterinarios\n");
 	printf("\t 4) Ranking de Veterinarios por Atenciones\n\n");
 	printf("\t 5) Cerrar la aplicacion\n");
 	printf("****************************************************\n");
-	printf("ingrese la opcion deseada: ");
+	printf("Ingrese la opcion deseada: ");
 	int opc;
 	scanf("%d", &opc);
 	return opc;
 }
 
-void regusuario(int &creg)
-{
+void regusuario(){
 	FILE *usuario = fopen("usuarios.dat", "a+b");
 	system("cls");
 	registro us;
 	bool u = false, p = false;
-	int mayus=0, digitos=0;
+	int mayus=0, digitos=0, opcion;
 
 	system("cls");
-
-	if(creg == 0)
-	{
+	printf("Elija usuario a crear(1.Usuario, 2.Administrador): ");
+	scanf("%d", &opcion);
+	if(opcion == 1)	us.modulo = 0;
+	else{	
+		us.modulo = 1;
 		printf("Cree usuario administrador\n\n\n");
 		_flushall();
 		do
@@ -99,16 +98,149 @@ void regusuario(int &creg)
 			}
 		}while(u == false);
 		
+		char contra[15], auxc[15], ull[10];
+		int cmin=0, cmay=0, cnum=0, a, b, c1, lon1, x;
+		bool ecar=false, econs=false, erorr=false, eletconsec=false, error, elconsec=false;
+		
 		system("cls");
 		printf("Condiciones:\n-Contener al menos una letra mayuscula, una minuscula y un numero\n-No contener ningun caracter de puntuacion, ni acentos, ni espacios\n-Solo contener caracteres alfanumericos\n-Tener entre 6 y 32 caracteres\n-No tener mas de 3 caracteres consecutivos\n-No tener 2 caracteres consecutivos que refieran a letras alfabeticamente consecutivas(ascendentemente)\n");
-		
+		printf("\nContraseña: ");
+		_flushall();
+		gets(contra);
+	do{
+		do{	
+		lon1 = strlen(contra);
+            strcpy(auxc,contra);
+            strlwr(auxc);
 
-		creg = 1;
-		fwrite(&us, sizeof(registro), 1, usuario);
-	}
-	else
-	{
+            if (lon1>=6 && lon1<=32)
+            {            
+                for (int i = 0; i < lon1; i++)
+                {
+                    //REQUISITO A
+                    if((contra[i]>='a') || (contra[i]<'z'))
+                    {
+                        cmin++;
+                        //error=true;
+                    }
+                    //REQUISITO A
+                    if ((contra[i]>='A') || (contra[i]<'Z'))
+                    {
+                        cmay++;
+                        //error=true;       
+                    }
+                    //REQUISITO A
+                    if (contra[i]>=48 && contra[i]<=57)
+                    {
+                        cnum++;
+                        //error=true;
+                    }
+                    
+                    //REQUISITO B
+                    if ((contra[i]<47 || contra[i]>58) && (contra[i]<65 || contra[i]>90) && (contra[i]<97 || contra[i]>122))
+                    {
+                        //error=true;
+                        ecar=true;
+                    }
+                    //REQUISITO D
+                    if (contra[i]>=48 && contra[i]<=57)
+                    {
+                        a=contra[i];
+                        b=a+1;
+                        c1=a+2;
+                        //contadorconsecutivo++;
+                        if (contra[i+1]==b && contra[i+2]==c1)
+                        {
+                            econs=true;
+                            //error=true;
+                        }                    
+                    } 
+                    
+                    //REQUISITO E
+                    
+                    if(auxc[i]>96 && auxc[i]<123)
+                    {                    
+                        a=auxc[i];
+                        b=a+1;
+                        if(auxc[i+1]==b)
+                        {
+                            //error=true;
+                            printf("\nca= %c",auxc[i]);
+                            printf("\na= %d",a);
+                            printf("\nca= %c",auxc[i+1]);
+                            printf("\nb= %d",b);
+                            eletconsec=true;
+                        }
+                    }               
+                }			
+                //MUESTRA ERROR  A
+                if (cmin==1)
+                {
+                    printf("\nDebe tener al menos una letra minuscula!!\n");
+                    error=true;
+                }
+                //MUESTRA ERROR  A
+                if (cmay<1)
+                {
+                    printf("\nDebe tener al menos una letra minuscula!!\n");
+                    error=true;
+                }
+                //MUESTRA ERROR  A
+                if (cnum<1)
+                {
+                    printf("\nDEBE TENER AL MENOS 1 NUMERO!!\n");
+                    error=true;
+                }
+                
+                //MUESTRA ERROR B
+                if(ecar==true)
+                {
+                    printf("\nDEBE CONTENER SOLO CARACTERES ALFANUMERICOS!!\n");
+                    error=true;
+                }
+                
+                
+                //MUESTRA ERROR  D
+                if (econs==true)
+                {
+                    printf("\nNO DEBE TENER MAS DE 3 NUMEROS CONSECUTIVOS!!\n");
+                    error=true;                
+                }
+                
+                //MUESTRA ERROR E
+                if (elconsec==true)
+                {
+                    printf("\nNO DEBE TENER LETRAS CONSECUTIVAS CONSECUTIVAS!!\n");
+                    error=true;
+                }            
+            } 
+            else
+            {
+                //REQUISITO-MUESTRA C
+                printf("\nDEBE INGRESAR ENTRE 6 Y 32 CARACTERES!!\n");
+                error=true;
+                system("pause");
+            }
+            system("pause");      
+        }while (error==true);
 
+        if (error==false)
+        {       
+            strcpy(us.Acontra, contra);
+            printf("\nINGRESE EL APELLIDO Y NOMBRE: ");        
+            _flushall();
+            gets(us.apynom);
+			fwrite(&us, sizeof(registro), 1, usuario);
+            do
+            {
+                printf("\nDESEA AGREGAR OTRO USUARIO (SI:1 , NO:0)");
+                scanf("%d",&x);
+            } while (x!=0 || x!=1);
+        }
+              
+    }while (x==1);
+	
 	}
+
 	fclose(usuario);
 }
